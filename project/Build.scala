8f32a31783c4c68b8cba6b34a0e90c77a3858371
Andrea Salvadore <andrea.salvadore@gmail.com>
2012-10-27 13:22:02 +0100

    added mongo dependencies and json file with some events

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,15 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+        "com.mongodb.casbah" %% "casbah" % "2.1.5-1"
       // Add your project dependencies here,
     )
+    
+    
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+    	resolvers += "sonatype repository" at "https://oss.sonatype.org/content/groups/scala-tools/" 
+     // Add your own project settings here      
     )
 
 }
