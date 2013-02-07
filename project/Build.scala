7eb7ce667d3074e2595525c79f121fb401e20470
andy <andy@hicks.net>
2013-02-07 11:08:36 +0000

    upgrade to Play2.1
    fixed dependencies on casbah

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -9,13 +9,19 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // "org.mongodb" % "casbah" % "2.5.0",
-      "com.github.tmingos" % "casbah_2.10" % "2.5.0-SNAPSHOT",
-      // "com.novus" %% "salat" % "1.9.1",
-      "se.radley" %% "play-plugins-salat" % "1.2-SNAPSHOT"
+//      "com.github.tmingos" % "casbah_2.10" % "2.5.0-SNAPSHOT",
+      "org.mongodb" % "casbah_2.10" % "2.5.0",
+//      "com.novus" %% "salat" % "1.9.1",
+      "com.novus" %% "salat-core" % "1.9.2-SNAPSHOT",
+      "com.novus" %% "salat-util" % "1.9.2-SNAPSHOT",
+      "se.radley" %% "play-plugins-salat" % "1.2-SNAPSHOT",
+      //"org.neo4j" % "neo4j" % "1.8.1",
+      "org.neo4j" % "neo4j-remote-graphdb" % "0.9-1.3.M01"
       )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "stylesheets" ** "main.less")
+      lessEntryPoints <<= baseDirectory(_ / "app" / "assets" / "stylesheets" ** "main.less"),
+        resolvers += "sonatype repository" at "http://oss.sonatype.org/content/repositories/snapshots"
   )
 
 }
