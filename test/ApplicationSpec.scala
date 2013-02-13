1497a3f25a103f21c33518c03897eb0b66c539b9
Thomas Schroeter <thomas@cliqz.com>
2013-02-13 14:20:41 +0100

    test rendering of template

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -9,11 +9,25 @@ import services.MockMeetingService
 class ApplicationSpec extends Specification {
 
   "Application" should {
-    
+
     "format Date with year and month" in new WithApplication {
       val d = new Date(0)
       val application = new Application(MockMeetingService)
       application.formatDate(d) must beEqualTo("1970 Jan")
+    }
+  }
+}
+
+// TODO: find a way to inject the MockMeetingService
+// or create FakeApplication
+class IndexPageSpec extends Specification {
+
+  "The index page" should {
+
+    "render index template" in new WithApplication {
+      val html = views.html.index()
+      contentType(html) must be equalTo("text/html")
+      contentAsString(html) must contain("London")
     }
   }
 }
